e96f49a56b582740c495cf34cb3d9df75e953ed4
Mika Rantanen <mika.rantanen@iki.fi>
2014-03-31 00:24:53 +0300

    Trackball improvements

--- app.ts
+++ app.ts
@@ -1,4 +1,5 @@
 ﻿/// <reference path="typings/three.d.ts" />
+/// <reference path="trackball.ts" />
 
 var WIDTH = 500;
 var HEIGHT = 500;
@@ -8,113 +9,52 @@ class Viewer {
     renderer: THREE.WebGLRenderer;
     camera: THREE.PerspectiveCamera;
     robot: THREE.Mesh;
-    currentRotation: THREE.Quaternion;
-    oldRotation: THREE.Quaternion;
-    mouseStartPos: THREE.Vector3;
+    
+    trackball: Trackball;
 
     prevTimestamp: number = null;
 
     constructor() {
-        this.mouseStartPos = new THREE.Vector3(0, 0, 0);
-        this.oldRotation = new THREE.Quaternion(0, 0, 0, 1);
-        this.currentRotation = new THREE.Quaternion(0, 0, 0, 1);
-
-        this.scene = new THREE.Scene();
-
-        this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: false });
+        this.renderer = new THREE.WebGLRenderer({ antialias: true });
         this.renderer.setSize(WIDTH, HEIGHT);
         this.renderer.setClearColor(0xeeeeee);
-        
+        document.body.appendChild(this.renderer.domElement);
 
-        var geometry = new THREE.TorusGeometry(300, 50, 100, 16);
+        this.scene = new THREE.Scene();
+        this.camera = new THREE.PerspectiveCamera(75, WIDTH / HEIGHT, 0.1, 10000);
+        this.trackball = new Trackball(this.camera, this.renderer.domElement);
+
+        this.animate = this.animate.bind(this);
+
+
+        var geometry = new THREE.TorusGeometry(300, 50, 100, 11);
         var material = new THREE.MeshLambertMaterial({ color: 0x00ff00 });
         this.robot = new THREE.Mesh(geometry, material);
-
-        var light = new THREE.DirectionalLight(0xffffff);
-        light.position.set(0, 0, 1000);
-
         this.scene.add(this.robot);
-        this.scene.add(light);
 
-
-        this.camera = new THREE.PerspectiveCamera(75, WIDTH / HEIGHT, 0.1, 10000);
-        this.camera.position.set(0, 0, 1000);
-        this.camera.lookAt(new THREE.Vector3(0, 0, 0));
-
-        document.body.appendChild(this.renderer.domElement);
-        this.onMouseDown = this.onMouseDown.bind(this);
-        this.onMouseUp = this.onMouseUp.bind(this);
-        this.onMouseMove = this.onMouseMove.bind(this);
-        this.onMouseLeave = this.onMouseLeave.bind(this);
-        this.renderer.domElement.addEventListener("mousedown", this.onMouseDown);
-        this.renderer.domElement.addEventListener("mouseleave", this.onMouseLeave);
-        this.renderer.domElement.addEventListener("mouseup", this.onMouseUp);
-    }
-
-    onMouseDown(e: MouseEvent) {
-        this.renderer.domElement.addEventListener("mousemove", this.onMouseMove);
-        this.mouseStartPos = this.calculatePoint(e.clientX, e.clientY);
-    }
-
-    onMouseUp() {
-        this.renderer.domElement.removeEventListener("mousemove", this.onMouseMove);
-        this.oldRotation = this.currentRotation.clone();
-    }
-
-    onMouseLeave() {
-        this.renderer.domElement.removeEventListener("mousemove", this.onMouseMove);
-        this.oldRotation = this.currentRotation.clone();
-    }
-
-    onMouseMove(e: MouseEvent) {
-        var p: THREE.Vector3 = this.calculatePoint(e.clientX, e.clientY);
-        var d = p.dot(this.mouseStartPos);
-        p.cross(this.mouseStartPos);
-
-        this.currentRotation.set(p.x, p.y, p.z, d)
-            .multiply(this.oldRotation).normalize();
-    }
-
-    calculatePoint(x: number, y: number) {
-        var p: THREE.Vector3 = new THREE.Vector3(
-            -((x / (WIDTH - 1)) * 2 - 1),
-            (y / (HEIGHT - 1)) * 2 - 1,
-            0);
-
-        if (p.lengthSq() <= 0.5) {
-            p.z = Math.sqrt(1 - p.lengthSq());
-        }
-        else {
-            p.z = 1.0 / (2 * Math.sqrt(p.lengthSq()));
-            p.normalize();
-        }
-
-        return p;
+        var light1 = new THREE.DirectionalLight(0xffffff);
+        light1.position.set(0, 0, 1000);
+        this.scene.add(light1);
     }
 
     animate(timestamp: number) {
-        this.render();
-
+        requestAnimationFrame(this.animate);
         this.update();
-
+        this.render();
         this.prevTimestamp = timestamp;
+    }
+
+    update() {
+        this.camera.quaternion = this.trackball.getRotation().clone();
+        this.camera.position = (new THREE.Vector3(0, 0, 1000)).applyQuaternion(this.trackball.getRotation());
     }
 
     render() {
         this.renderer.render(this.scene, this.camera);
     }
-
-    update() {
-        this.robot.quaternion = this.currentRotation;
-    }
 }
 
 window.onload = () => {
     var viewer: Viewer = new Viewer();
-
-    function viewerLoop(timestamp: number) {
-        requestAnimationFrame(viewerLoop);
-        viewer.animate(timestamp);
-    }
-    requestAnimationFrame(viewerLoop);
+    requestAnimationFrame(viewer.animate);
 }
---
+++ trackball.ts
@@ -0,0 +1,75 @@
+﻿/// <reference path="typings/three.d.ts" />
+
+// Implements a simple virtual trackball
+class Trackball {
+    private camera: THREE.Camera;
+    private element: HTMLElement;
+
+    private currentRotation: THREE.Quaternion;
+    private oldRotation: THREE.Quaternion;
+    private dragStartPos: THREE.Vector3;
+
+    constructor(camera: THREE.Camera, element: HTMLElement) {
+        this.camera = camera;
+        this.element = element;
+
+        this.oldRotation = new THREE.Quaternion(0, 0, 0, 1);
+        this.currentRotation = new THREE.Quaternion(0, 0, 0, 1);
+        this.dragStartPos = new THREE.Vector3(0, 0, 0);
+
+        // Setup event listeners
+        this.onMouseDown = this.onMouseDown.bind(this);
+        this.onMouseUp = this.onMouseUp.bind(this);
+        this.onMouseMove = this.onMouseMove.bind(this);
+        this.element.addEventListener("mousedown", this.onMouseDown, false);
+        this.element.addEventListener("mouseup", this.onMouseUp, false);
+        this.element.addEventListener("mouseleave", this.onMouseUp, false);
+    }
+
+    // Returns a rotation quaternion
+    public getRotation(): THREE.Quaternion {
+        return this.currentRotation;
+    }
+
+    private onMouseDown(e: MouseEvent) {
+        this.element.addEventListener("mousemove", this.onMouseMove);
+        e.preventDefault();
+
+        this.dragStartPos = this.calculatePoint(e.clientX, e.clientY);
+    }
+
+    private onMouseUp(e: MouseEvent) {
+        this.element.removeEventListener("mousemove", this.onMouseMove);
+        e.preventDefault();
+
+        this.oldRotation = this.currentRotation.clone();
+    }
+
+    private onMouseMove(e: MouseEvent) {
+        e.preventDefault();
+
+        var p: THREE.Vector3 = this.calculatePoint(e.clientX, e.clientY);
+        var d = p.dot(this.dragStartPos);
+        p.cross(this.dragStartPos);
+
+        this.currentRotation = this.oldRotation.clone()
+            .multiply(new THREE.Quaternion(p.x, p.y, p.z, d)).normalize();
+    }
+
+    private calculatePoint(x: number, y: number) {
+        var p: THREE.Vector3 = new THREE.Vector3(
+            (x / this.element.offsetWidth) * 2 - 1,
+            -((y / this.element.offsetHeight) * 2 - 1),
+            0);
+
+        if (p.lengthSq() <= 0.5) {
+            p.z = Math.sqrt(1 - p.lengthSq());
+        }
+        else {
+            p.z = 1.0 / (2 * Math.sqrt(p.lengthSq()));
+            p.normalize();
+        }
+
+        return p;
+    }
+} 
